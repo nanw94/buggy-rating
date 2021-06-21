f077442be998bb3c4b37e0ad05cd0d9d3e1a915f
Nathan Wang <nathan.w@clubware.co.nz>
2021-06-21 22:49:43 +1200

    add retry and comment

--- cypress/integration/Tests.js
+++ cypress/integration/Tests.js
@@ -5,7 +5,6 @@ import * as home from '../page/home';
 
 let userToSignUp;
 let user;
-let userToOverVote;
 
 context('pre-login tests', ()=>{
     it('should block voting without login',()=>{
@@ -31,39 +30,57 @@ context('pre-login tests', ()=>{
 })
 
 context('post-login tests',()=>{
-    //before all, sign a user up for post-login behaviour
+    //Before all, sign a user up for post-login behaviour
     before('sign up a user', ()=>{
         user = common.newUser();
         common.signup(user);
     });
 
-    //before each test, login by set token to brower
+    //Before each test, login by set token to brower
     beforeEach('login with the new user',()=>{
         common.login(user.userName, user.password);
     })
 
     it('should allow user to comment and vote',()=>{
-        common.getRandomModelId().then(id=>{    //randomly pick up a model to vote
-            common.getModelVoteCount(id).then(count=>{      //get the original vote count in advance
-                model.navigateTo(id);        //navigate to the model detail page
-                model.setComment(user.comment);     //input comment
-                model.getVoteBtn().click();     //click vote button
+        //Randomly pick up a model to vote
+        common.getRandomModelId().then(id=>{    
+            //Get the original vote count in advance
+            common.getModelVoteCount(id).then(count=>{
+                //Navigate to the model detail page
+                model.navigateTo(id);
+                //Input comment       
+                model.setComment(user.comment);     
+                //Click vote button
+                model.getVoteBtn().click();
+                //Verify 'Thank you for your vote!'
                 cy.contains('Thank you for your vote!').should('be.visible');
-                model.getLastComment().should('contain',user.comment)       //the comment should be logged on top
-                cy.contains('strong', (count+1).toString()).should('be.visible');       //vote count should get increased
-                // .and('contain',user.firstName);      //currently there is a bug about missing the author for model with a big vote count, so it could fail.
+                //The comment should be logged on top
+                model.getLastComment().should('contain',user.comment)
+                //The vote count should get increased by 1
+                cy.contains('strong', (count+1).toString()).should('be.visible');
+                //currently there is a bug about missing the author for model with a big vote count, so it could fail.      
+                // .and('contain',user.firstName);      
             })            
         })
     });
 
-    it('should block voting on the same model twice',()=>{
-        userToOverVote = common.newUser();         //need a new user to avoid testing on an model that already voted
-        common.signup(userToOverVote);
-        common.login(userToOverVote.userName,userToOverVote.password);
+    it('should block voting on the same model twice', {
+        //This test will be retried in case that the model that randomly picked up has already been voted (if the api returns error "message": "Cannot vote more than once")
+        retries: {
+          runMode: 3,
+          openMode: 2,
+        },
+      },
+      ()=>{
+        //vote the model once via api to make sure it's already voted by the current user
+        //Randomly pick up a model
         common.getRandomModelId().then(id=>{
-            common.vote(userToOverVote,id,'this was voted by api');         //vote the model once via api to make sure it's already voted by the current user
+            //Vote the model once (through api)
+            common.vote(user,id,'this was voted by api');
+            //Open the model detail page in UI
             model.navigateTo(id);
             cy.contains('Thank you for your vote!').should('be.visible');
+            //Vote button shouldn't be there any more
             cy.contains('button','Vote!').should('not.exist');
         })
     });
